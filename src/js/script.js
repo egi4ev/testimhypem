53c066153e41b59e09d834bf38f81f2e29e140ee
egg <egor.izh18@gmail.com>
2022-05-19 23:18:50 +0300

    adaptive section menu fuuuuuuuuuuuuuck it was hard O_o

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('nav .menu__nav'),
+    menuItem = document.querySelectorAll('.menu__nav li'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
